eb83ef9a3910ba93acf0195fdc381d64416198db
John <John@DESKTOP>
2023-02-05 13:01:35 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        //Os atribuitos aqui referenciarão os campos no Banco de Dados
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
